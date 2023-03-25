ea967ec4276334f870f1e7f1617cbc765b4fdee2
Kamil Prokop <zara.pug.yt@gmail.com>
2023-03-25 21:49:00 +0100

    many-to-many table created

--- home-library-task-catalogue/home-library-main.py
+++ home-library-task-catalogue/home-library-main.py
@@ -1,36 +1,31 @@
 import sqlite3
 
 #creating 1 database with 3 tables: books, authors, library_status
-connection = sqlite3.connect("home-library.db")
+db_file = "home-library.db"
+connection = sqlite3.connect(db_file)
+connection.execute('''PRAGMA foreign_keys=ON''')
 cursor = connection.cursor()
 
 #table concerning books: title
 sql_books = """
 CREATE TABLE IF NOT EXISTS table_books(
-    id INTEGER PRIMARY KEY,
-    book_title TEXT NOT NULL
+    book_title TEXT PRIMARY KEY NOT NULL
 );
 """
 
 #table concerning authors: name, surname
 sql_authors = """
 CREATE TABLE IF NOT EXISTS table_authors(
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    surname TEXT NOT NULL
+    name TEXT PRIMARY KEY NOT NULL
 );
 """
 
 #table library as combo(many2many): books, name, surname, availability (2 statuses: YER or NO)  
 sql_library = """
 CREATE TABLE IF NOT EXISTS table_library(
-    books_title TEXT NOT NULL,
-    authors_name TEXT NOT NULL,
-    authors_surname TEXT NOT NULL,
-    availability VARCHAR(3) NOT NULL,
-    FOREIGN KEY (books_title) REFERENCES table_books (book_title),
-    FOREIGN KEY (authors_name) REFERENCES table_authors (name),
-    FOREIGN KEY (authors_surname) REFERENCES table_authors (surname)
+    books_title TEXT NOT NULL REFERENCES table_books (book_title),
+    authors_name TEXT NOT NULL REFERENCES table_authors (name),
+    availability VARCHAR(3) NOT NULL
 );
 """
 
@@ -43,4 +38,43 @@ connection.commit()
 cursor.execute(sql_library)
 connection.commit()
 
+#inserting data into tables
+#connection = sqlite3.connect(db_file)
+#connection.execute('''PRAGMA foreign_keys=ON''')
+
+#cursor = connection.cursor()
+
+#cursor.execute('''PRAGMA foreign_keys=ON''')
+
+cursor.execute('''INSERT OR IGNORE INTO table_books (book_title) VALUES ("Propaganda")''')
+cursor.execute('''INSERT OR IGNORE INTO table_books (book_title) VALUES ("Inżynieria finansowa")''')
+cursor.execute('''INSERT OR IGNORE INTO table_books (book_title) VALUES ("Działa Nawarony")''')
+cursor.execute('''INSERT OR IGNORE INTO table_books (book_title) VALUES ("Tylko dla orłów")''')
+
+connection.commit()
+
+
+cursor.execute('''INSERT OR IGNORE INTO table_authors (name) VALUES ("Edward Bernays")''')
+cursor.execute('''INSERT OR IGNORE INTO table_authors (name) VALUES ("Rafał Weron")''')
+cursor.execute('''INSERT OR IGNORE INTO table_authors (name) VALUES ("Alistair MacLean")''')
+connection.commit()
+
+
+cursor.execute('''INSERT OR IGNORE INTO table_library (books_title, authors_name, availability) VALUES ("Propaganda", "Edward Bernays", "NO")''')
+cursor.execute('''INSERT OR IGNORE INTO table_library (books_title, authors_name, availability) VALUES ("Inżynieria finansowa", "Rafał Weron", "YES")''')
+cursor.execute('''INSERT OR IGNORE INTO table_library (books_title, authors_name, availability) VALUES ("Działa Nawarony", "Alistair MacLean", "NO")''')
+cursor.execute('''INSERT OR IGNORE INTO table_library (books_title, authors_name, availability) VALUES ("Tylko dla orłów", "Alistair MacLean", "YES")''')
+
+
+connection.commit()
+
+
+connection = sqlite3.connect(db_file)
+cursor = connection.cursor()
+sql_select_books = f"SELECT * FROM table_books"
+sql_select_authors = f"SELECT * FROM table_authors"
+sql_select_library = f"SELECT * FROM table_library"
+cursor.execute(sql_select_library)
+results = cursor.fetchall()
+print(results)
 connection.close()
